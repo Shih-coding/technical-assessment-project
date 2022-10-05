5bea7b5bd8e2b5dfc3e439485b10be40bff0fca2
Shih-coding <100893145+Shih-coding@users.noreply.github.com>
2022-10-05 13:24:56 +0800

    added structure of landing page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from "react";
-import "./App.css";
+import "./App.css"
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <footer>
-          This project was coded by Rebecca Shih and is {""}
-          <a
-            href="https://github.com/Shih-coding/weather-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-        </footer>
+        <div className="row">
+          <div className="col-6">
+            <img
+              className="img-fluid"
+              src={require("./assets/event-img.png")}
+            />
+          </div>
+          <div className="col-6">
+            <div className="main-text">
+            <h1>
+              Imagine if <span className="snapchat">Snapchat</span> had events
+            </h1>
+            <h2>
+              Easily host and share events with your friends across any social
+              media.
+              </h2>
+              </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );

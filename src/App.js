e44ddc975c7c7b69500f320916309827db3ffc4b
Shih-coding <100893145+Shih-coding@users.noreply.github.com>
2022-10-05 11:18:25 +0800

    added footer

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        
-    </div>
+        <footer>
+          This project was coded by Rebecca Shih and is {""}
+          <a
+            href="https://github.com/Shih-coding/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
